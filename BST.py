150383a9aba18aae778d7094d0decd9193daff8e
moppr <mantony42@gmail.com>
2020-02-25 14:35:18 -0500

    Set up empty methods and driver in BST.py

---
+++ BST.py
@@ -0,0 +1,26 @@
+class BSTRec:
+
+    def __init__(self):
+        pass
+
+    def insert_rec(self):
+        pass
+
+    def delete_rec(self):
+        pass
+
+    def find_next_rec(self):
+        pass
+
+    def find_prev_rec(self):
+        pass
+
+    def find_min_rec(self):
+        pass
+
+    def find_max_rec(self):
+        pass
+
+
+if __name__ == "__main__":
+    print("hi")
